96616fb157a0b4c210cb71004b2b84436c664377
Ru <rsrazonable1@up.edu.ph>
2021-08-18 12:01:40 +0800

    refactoring

---
+++ src/main/java/com/rosalieraz/importmodule/repository/EventsRepository.java
@@ -0,0 +1,11 @@
+package com.rosalieraz.importmodule.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.rosalieraz.importmodule.model.Events;
+
+@Repository
+public interface EventsRepository extends JpaRepository<Events, Integer> {
+
+}

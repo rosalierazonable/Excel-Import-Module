da9f313254ac17c472886504cd81482c8a976566
Ru <rsrazonable1@up.edu.ph>
2021-08-18 12:03:14 +0800

    PostMapping for addEvents

--- src/main/java/com/rosalieraz/importmodule/controller/ExcelController.java
+++ src/main/java/com/rosalieraz/importmodule/controller/ExcelController.java
@@ -1,12 +1,17 @@
 package com.rosalieraz.importmodule.controller;
 
 import java.io.IOException;
+/*import java.io.IOException;
+import java.util.HashMap;*/
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.rosalieraz.importmodule.model.Events;
+import com.rosalieraz.importmodule.repository.EventsRepository;
 import com.rosalieraz.importmodule.service.FileHandlingService;
 
 @RestController
@@ -15,17 +20,30 @@ public class ExcelController {
 	@Autowired
 	private FileHandlingService fhService;
 	
+	@Autowired
+	private EventsRepository eRepository;
+
+	
+	
+	/*
+	 * Get List of Files
+	 */
+	
 	@GetMapping("/file-list")
 	public List<String> getFileList () {
 		return fhService.getFileList();
 	}
 	
-	@GetMapping("/log-excel")
-	public void readExcel () {
-		try {
-			fhService.readingExcel();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	
+	
+	/*
+	 * Add Events from Excel
+	 */
+	
+	@PostMapping("/addEvents") 
+	public void getEvents () throws IOException { 
+		List<List<Events>> eventsList = fhService.loopThroughFiles();
+		for(List<Events> events: eventsList)
+			eRepository.saveAll(events);
 	}
 }

fe67d4447cb65b581b2e169b4b9ae9099babdae5
Ru <rsrazonable1@up.edu.ph>
2021-08-19 11:30:57 +0800

    refactor

--- src/main/java/com/rosalieraz/importmodule/service/FileHandlingService.java
+++ src/main/java/com/rosalieraz/importmodule/service/FileHandlingService.java
@@ -1,8 +1,9 @@
 package com.rosalieraz.importmodule.service;
 
 import java.io.IOException;
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
@@ -14,10 +15,14 @@ public interface FileHandlingService {
 	
 	String getExtension (String fileName);
 	
-	HashMap<String, Object> readingConfigDetails (XSSFWorkbook workbook ) throws IOException;
+	Map<String, Object> readingConfigDetails (XSSFWorkbook workbook ) throws IOException;
 	
-	List<Events> readingEventsTable (XSSFWorkbook workbook) throws IOException;
+	List<Events> readingEventsTable (XSSFWorkbook workbook, ArrayList<String> eventFields) throws IOException;
 	
 	List<List<Events>> loopThroughFiles () throws IOException;
+	
+	Map<String, Integer> readingEnums (XSSFWorkbook workbook, String field);
+	
+//	void writeIntoExcel ();
 	
 }

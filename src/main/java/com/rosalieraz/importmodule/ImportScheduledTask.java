b2a3775bbd6751f04bafa13e7216a0051bea7001
Ru <rsrazonable1@up.edu.ph>
2021-08-20 18:40:29 +0800

    implement task scheduling

---
+++ src/main/java/com/rosalieraz/importmodule/ImportScheduledTask.java
@@ -0,0 +1,27 @@
+package com.rosalieraz.importmodule;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import com.rosalieraz.importmodule.service.FileHandlingService;
+
+@Component
+public class ImportScheduledTask {
+
+	@Autowired
+	FileHandlingService fhService;
+	
+	@Scheduled(cron = "0 15 15 * * 1-5")
+	public void importExcel () {
+		
+		try {
+			fhService.loopThroughFiles();
+		} catch (IOException e) {
+			e.getMessage();
+		}
+	}
+	
+}

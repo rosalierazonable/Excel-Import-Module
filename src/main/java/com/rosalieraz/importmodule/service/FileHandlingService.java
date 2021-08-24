4cea1aab4e8cd2576c8b45541212ffb13af6696c
Ru <rsrazonable1@up.edu.ph>
2021-08-24 10:46:46 +0800

    add methods for reading the remaining tables

--- src/main/java/com/rosalieraz/importmodule/service/FileHandlingService.java
+++ src/main/java/com/rosalieraz/importmodule/service/FileHandlingService.java
@@ -2,9 +2,13 @@ package com.rosalieraz.importmodule.service;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+import javax.validation.ConstraintViolationException;
+
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public interface FileHandlingService {
@@ -22,8 +26,22 @@ public interface FileHandlingService {
 	void moveFile (String fileName, String targetDirectory);
 
 	List<Object[]>  writeIntoExcel (List<Object[]> errors) throws IOException;
+	
+	Iterator<Row> loadingIterator (XSSFWorkbook workbook);
 
-	List<Object[]> readingEventsTable (XSSFWorkbook workbook, ArrayList<String> eventFields, String identifier) throws IOException;
+	List<Object[]> readingEventsTable (XSSFWorkbook workbook, ArrayList<String> eventFields, Map<String, Object> config) throws ConstraintViolationException;
+
+	List<Object[]> readingPartnersTable (XSSFWorkbook workbook, ArrayList<String> partnerFields, Map<String, Object> config) throws ConstraintViolationException;
+	
+	List<Object[]> readingEmergencyNosTable (XSSFWorkbook workbook, ArrayList<String> eNumFields, Map<String, Object> config) throws ConstraintViolationException;
+
+	List<Object[]> readingPaymentsTable (XSSFWorkbook workbook, ArrayList<String> paymentFields, Map<String, Object> config) throws ConstraintViolationException;
+
+	List<Object[]> readingInterestsTable (XSSFWorkbook workbook, ArrayList<String> interestFields, Map<String, Object> config) throws ConstraintViolationException;
+	
+	List<Object[]> readingEATable (XSSFWorkbook workbook, ArrayList<String> eAFields, Map<String, Object> config) throws ConstraintViolationException;
+
+	List<Object[]> readingGATable (XSSFWorkbook workbook, ArrayList<String> gAFields, Map<String, Object> config) throws ConstraintViolationException;
 	
 	void loopThroughFiles () throws IOException;
 	

81e7e4bb0e7b15f737d31878a75c53ae5b93e6ba
Ru <rsrazonable1@up.edu.ph>
2021-08-24 19:00:54 +0800

    refactors

--- src/main/java/com/rosalieraz/importmodule/repository/EventAttendanceRepository.java
+++ src/main/java/com/rosalieraz/importmodule/repository/EventAttendanceRepository.java
@@ -3,9 +3,9 @@ package com.rosalieraz.importmodule.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.rosalieraz.importmodule.model.Events;
+import com.rosalieraz.importmodule.model.EventAttendance;
 
 @Repository
-public interface EventAttendanceRepository extends JpaRepository<Events, Integer> {
+public interface EventAttendanceRepository extends JpaRepository<EventAttendance, Integer> {
 
 }
